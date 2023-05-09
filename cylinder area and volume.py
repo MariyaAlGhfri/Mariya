fa3fa7a057ba867ceb112e19aceea54b4f21283e
MariyaALGhafri <amariya353@gmail.com>
2023-05-09 13:29:48 +0400

    day2

---
+++ cylinder area and volume.py
@@ -0,0 +1,13 @@
+
+
+h=4
+r=6
+PI=22/7
+
+area=2*PI*r*h+2*PI*r**2
+
+volume=PI*r**2*h
+
+
+print("area is : ",round(area,2))
+print("volume is : ",round(volume,2))
---
+++ day2.py
@@ -0,0 +1,20 @@
+
+
+
+b=23
+r=14
+n=2
+y=b*((1+r/100)**n)
+print(y)
+
+a=9
+e=5
+t=90
+c =sqrt(a**2+e**2–2*a*e*cos(t))
+print(c)
+
+number=434.99999999
+x=int(number*1000)/1000
+print(x)
+
+
